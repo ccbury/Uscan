389da496dfe0c8275ef8004b0a2cd5206cd16c74
ccbury <conordebru@gmail.com>
2019-10-20 14:12:45 +0100

    Tabs set up with classes and XML files for design.

--- app/src/main/java/com/scanners/uscan/SearchFragment.java
+++ app/src/main/java/com/scanners/uscan/SearchFragment.java
@@ -1,7 +1,17 @@
 package com.scanners.uscan;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
+    View myView;
 
-}//End class
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.search_fragment, container, false);
+        return myView;
+
+    }//End onCreateView
+}
